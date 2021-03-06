49fd9d24aabed377a69e04f541b4b22d90e90b4f
Ivan Kremnev <iakremnev@mail.ru>
2021-03-06 14:44:41 +0300

    not yet json serializable, use pickle

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 from fastapi import FastAPI
 from fastapi import Request
-# from pydantic import BaseModel
 
-import json
+import pickle
 import datetime
-import os
 
 app = FastAPI()
 
-os.makedirs("requests", exist_ok=True)
 
 @app.post("/")
 async def root_get(request: Request):
     with open(f"requests/GET {datetime.datetime.now()}.json", "w") as f:
-        json.dump(f, request.json(), indent=2)
+        pickle.dump(f, request)
 
 
 @app.get("/")
 async def root_post(request: Request):
     with open(f"requests/POST {datetime.datetime.now()}.json", "w") as f:
-        json.dump(f, request.json(), indent=2)
+        pickle.dump(f, request)
