8a421fd96a5edd231ac79042c89652fc9fa8d714
Ivan Kremnev <iakremnev@mail.ru>
2021-03-06 14:51:29 +0300

    fix paths

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from fastapi import Request
 
 import pickle
 import datetime
+import os
 
 app = FastAPI()
 
 
 @app.post("/")
 async def root_get(request: Request):
+    os.makedirs("requests", exist_ok=True)
     with open(f"requests/GET {datetime.datetime.now()}.json", "w") as f:
         pickle.dump(f, request)
 
 
 @app.get("/")
 async def root_post(request: Request):
+    os.makedirs("requests", exist_ok=True)
     with open(f"requests/POST {datetime.datetime.now()}.json", "w") as f:
         pickle.dump(f, request)
