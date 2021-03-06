9febf6a0e776cfed7ce27a22f4758a01619d42e4
Ivan Kremnev <iakremnev@mail.ru>
2021-03-06 14:39:36 +0300

    dump requests to files

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from fastapi import Request
 # from pydantic import BaseModel
+
+import json
+import datetime
+import os
 
 app = FastAPI()
 
+os.makedirs("requests", exist_ok=True)
+
 @app.post("/")
-async def root_get():
-    pass
+async def root_get(request: Request):
+    with open(f"requests/GET {datetime.datetime.now()}.json", "w") as f:
+        json.dump(f, request.json(), indent=2)
 
 
 @app.get("/")
-async def root_post():
-    pass
+async def root_post(request: Request):
+    with open(f"requests/POST {datetime.datetime.now()}.json", "w") as f:
+        json.dump(f, request.json(), indent=2)
