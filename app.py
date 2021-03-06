7a72d977f0b7617d54e36635b090f86bee820432
Ivan Kremnev <iakremnev@mail.ru>
2021-03-06 15:35:25 +0300

    fix wb file format

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ app = FastAPI()
 @app.post("/")
 async def root_get(request: Request):
     os.makedirs("requests", exist_ok=True)
-    with open(f"requests/GET {datetime.datetime.now()}.json", "w") as f:
-        pickle.dump(f, request)
+    with open(f"requests/GET {datetime.datetime.now()}.pickle", "wb") as f:
+        pickle.dump(request, f)
 
 
 @app.get("/")
 async def root_post(request: Request):
     os.makedirs("requests", exist_ok=True)
-    with open(f"requests/POST {datetime.datetime.now()}.json", "w") as f:
-        pickle.dump(f, request)
+    with open(f"requests/POST {datetime.datetime.now()}.pickle", "wb") as f:
+        pickle.dump(request, f)
